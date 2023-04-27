b46a65e8130c616ccde7f3035276cf43c26db7e6
gsidsid <gsidsid@gmail.com>
2023-04-27 16:52:34 -0700

    Resolve server url for swagger

--- src/util.ts
+++ src/util.ts
@@ -22,8 +22,8 @@ interface SwaggerSpec {
   security: { apiKeyHeader: any[] }[];
 }
 
-const serverURL = process.env.RENDER_EXTERNAL_HOSTNAME
-  ? `https://${process.env.RENDER_EXTERNAL_HOSTNAME}`
+const serverURL = process.env.PAYLOAD_PUBLIC_RENDER_EXTERNAL_HOSTNAME
+  ? `https://${process.env.PAYLOAD_PUBLIC_RENDER_EXTERNAL_HOSTNAME}`
   : "http://localhost:3000";
 
 /**
