ff903337089e6df89a175f3bd2381fe61d1b4a17
gsidsid <gsidsid@gmail.com>
2023-04-27 23:36:14 -0700

    Note render deployment time

--- src/util.ts
+++ src/util.ts
@@ -25,8 +25,8 @@ interface SwaggerSpec {
   security: { apiKeyHeader: any[] }[];
 }
 
-const serverURL = process.env.PAYLOAD_PUBLIC_RENDER_EXTERNAL_HOSTNAME
-  ? `https://${process.env.PAYLOAD_PUBLIC_RENDER_EXTERNAL_HOSTNAME}`
+const serverURL = process.env.PAYLOAD_PUBLIC_EXTERNAL_HOSTNAME
+  ? `https://${process.env.PAYLOAD_PUBLIC_EXTERNAL_HOSTNAME}`
   : "http://localhost:3000";
 const mongoURL =
   process.env.PAYLOAD_PUBLIC_MONGODB_URI || "mongodb://localhost/payload";
