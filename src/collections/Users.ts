6d0a8a93561899852c6970fb4f621d8643849e47
gsidsid <gsidsid@gmail.com>
2023-05-01 11:30:26 -0700

    try rbac package defaults

--- src/collections/Users.ts
+++ src/collections/Users.ts
@@ -14,22 +14,6 @@ const Users: CollectionConfig = {
       name: "name",
       type: "text",
     },
-    {
-      name: "role",
-      type: "select",
-      options: [
-        { label: "Viewer", value: "viewer" },
-        { label: "Editor", value: "editor" },
-        { label: "Admin", value: "admin" },
-      ],
-      defaultValue: "editor",
-      saveToJWT: true,
-      required: true,
-      admin: {
-        description:
-          "The role this user belongs to. Admins can manage users, editors can do everything else, and viewers can read data and try endpoints in the AIx UI, but do not have access to your API key.",
-      },
-    },
   ],
 };
 
